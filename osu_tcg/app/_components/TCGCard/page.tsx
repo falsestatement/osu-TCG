49b0facc5ac9ee1086a308dfe2242f737126db8b
Adrian Cheng <bluepop11@hotmail.com>
2024-05-04 13:13:05 -0400

    feat: animated stacked deck layout

--- osu_tcg/app/_components/TCGCard/page.tsx
+++ osu_tcg/app/_components/TCGCard/page.tsx
@@ -1,6 +1,6 @@
 const TCGCard = () => {
 	return (
-		<div className="min-w-44 min-h-64 w-44 h-64 bg-gradient-to-br from-pink-400 to-violet-600 m-2 rounded-lg p-1 ">
+		<div className="[flex -ml-20 first:ml-0 min-w-44 min-h-64 w-44 h-64 bg-gradient-to-br from-pink-400 to-violet-600 rounded-lg p-1 drop-shadow-[0_5px_5px_rgba(0,0,0,0.35)] transition ease-in-out duration-200 hover:-translate-y-2 peer peer-hover:relative peer-hover:transition peer-hover:ease-in-out peer-hover:duration-200 peer-hover:translate-x-20">
 			<div className="bg-pink-300 w-full h-full rounded-md p-2">
 				<div className="bg-gradient-to-r from-pink-500 to-red-500 rounded-md flex justify-center items-center border-solid border-2 border-pink-700">
 					Card Name
--- osu_tcg/app/page.tsx
+++ osu_tcg/app/page.tsx
@@ -1,13 +1,21 @@
-import TCGCard from './_components/TCGCard/page.tsx'
+"use client"
+import TCGCard from './_components/TCGCard/page.tsx';
+import { useState } from 'react';
 export default function Home() {
+	const [numRed, setNumRed] = useState(0);
+	const [numBlue, setNumBlue] = useState(0);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    	<h1>Hello World</h1>
-	<TCGCard/>
-	<TCGCard/>
-	<TCGCard/>
-	<TCGCard/>
-	<TCGCard/>
+    <main className="min-h-screen items-center justify-between">
+    	<button onClick={() => setNumRed(numRed + 1)}>Add Red Card</button>
+    	<button onClick={() => setNumBlue(numBlue + 1)}>Add Blue Card</button>
+	<div className="grid grid-cols-2">
+		<div className="flex flex-row bg-red-200">
+			{[...Array(numRed)].fill(undefined).map((_) => <TCGCard/>)}
+		</div>
+		<div className="flex flex-row bg-blue-200">
+			{[...Array(numBlue)].fill(undefined).map((_) => <TCGCard/>)}
+		</div>
+	</div>
     </main>
   );
 }
