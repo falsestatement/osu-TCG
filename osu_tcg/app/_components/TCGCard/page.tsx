83d4f469e2ca8ae0fb112d7b402023b59ba97023
Adrian Cheng <bluepop11@hotmail.com>
2024-05-04 22:13:31 -0400

    fix: hover group assigned to outer border container

--- osu_tcg/app/_components/TCGCard/page.tsx
+++ osu_tcg/app/_components/TCGCard/page.tsx
@@ -3,8 +3,8 @@ import { useState } from 'react';
 const TCGCard = () => {
 	const [counter, setCounter] = useState(0);
 	return (
-		<div className="[flex -ml-32 first:ml-0 min-w-44 min-h-64 w-44 h-64 bg-gradient-to-br from-pink-400 to-violet-600 rounded-lg p-1 drop-shadow-[0_5px_5px_rgba(0,0,0,0.35)] transition ease-in-out duration-200 hover:-translate-y-2 peer peer-hover:relative peer-hover:transition peer-hover:ease-in-out peer-hover:duration-200 peer-hover:translate-x-32">
-			<div className="group flex flex-col justify-between bg-pink-300 w-full h-full rounded-md p-2">
+		<div className="group [flex -ml-32 first:ml-0 min-w-44 min-h-64 w-44 h-64 bg-gradient-to-br from-pink-400 to-violet-600 rounded-lg p-1 drop-shadow-[0_5px_5px_rgba(0,0,0,0.35)] transition ease-in-out duration-200 hover:-translate-y-2 peer peer-hover:relative peer-hover:transition peer-hover:ease-in-out peer-hover:duration-200 peer-hover:translate-x-32">
+			<div className="flex flex-col justify-between bg-pink-300 w-full h-full rounded-md p-2">
 				<button className="absolute hidden text-white bg-pink-700 group-hover:flex -right-1 -top-1 bg-white w-5 h-5 justify-center items-center rounded-full text-sm hover:bg-pink-500">x</button>
 				<div className="flex flex-col grow">
 					<div className="bg-gradient-to-r from-pink-500 to-red-500 rounded-md flex justify-center items-center border-solid border-2 border-pink-700">
