1d5fa8bd33a5e221a24ea6508616e533ff2ebe7e
Adrian Cheng <bluepop11@hotmail.com>
2024-05-04 21:21:34 -0400

    feat: card counters

--- osu_tcg/app/_components/TCGCard/page.tsx
+++ osu_tcg/app/_components/TCGCard/page.tsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
+
 const TCGCard = () => {
+	const [counter, setCounter] = useState(0);
 	return (
 		<div className="[flex -ml-32 first:ml-0 min-w-44 min-h-64 w-44 h-64 bg-gradient-to-br from-pink-400 to-violet-600 rounded-lg p-1 drop-shadow-[0_5px_5px_rgba(0,0,0,0.35)] transition ease-in-out duration-200 hover:-translate-y-2 peer peer-hover:relative peer-hover:transition peer-hover:ease-in-out peer-hover:duration-200 peer-hover:translate-x-32">
-			<div className="bg-pink-300 w-full h-full rounded-md p-2">
-				<div className="bg-gradient-to-r from-pink-500 to-red-500 rounded-md flex justify-center items-center border-solid border-2 border-pink-700">
-					Card Name
-				</div>
-				<div className="bg-white my-2 h-2/5 rounded-md flex justify-center items-center">
-					image here
-				</div>
+			<div className="group flex flex-col justify-between bg-pink-300 w-full h-full rounded-md p-2">
+				<div>
+					<div className="bg-gradient-to-r from-pink-500 to-red-500 rounded-md flex justify-center items-center border-solid border-2 border-pink-700">
+						Card Name
+					</div>
+					<div className="bg-white my-2 h-2/5 rounded-md flex justify-center items-center">
+						image here
+					</div>
 
-				<div className="text-[10px] text-center">
-					Some very long descriptor text
-					here including some random information
-					about the specified card etc etc
+					<div className="text-[10px] text-center">
+						Some very long descriptor text
+						here including some random information
+						about the specified card etc etc
+					</div>
+				</div>
+				<div className="group-hover:flex group-hover:justify-between">
+					<button onClick={() => setCounter((prev) => prev > 0 ? prev - 1 : prev)} className="p-2 mr-2 hidden bg-pink-200 rounded-lg w-5 h-5 group-hover:flex justify-center items-center hover:bg-pink-100">{"-"}</button>
+					<input value={counter} onChange={(e) => setCounter(+e.target.value)} type="number" className="w-full group-hover:text-center rounded-lg bg-transparent [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none "/>
+					<button onClick={() => setCounter((prev) => prev + 1)} className="p-2 ml-2 hidden bg-pink-200 rounded-lg w-5 h-5 group-hover:flex justify-center items-center hover:bg-pink-100">{"+"}</button>
 				</div>
 
 			</div>
