339d16a2b6d7323b6c8c206b420caf9c7980f574
Adrian Cheng <bluepop11@hotmail.com>
2024-05-09 05:21:16 -0400

    feat: card autoscaling

--- osu_tcg/app/_components/TCGCard/page.tsx
+++ osu_tcg/app/_components/TCGCard/page.tsx
@@ -39,10 +39,10 @@ const TCGCard = () => {
 				MouseY: normY,
 			});
 		}}
-		className="group hover:[transform:perspective(500px)_rotateY(var(--rot-y))_rotateX(var(--rot-x))] relative [flex -ml-[62px] first:ml-0 min-w-44 min-h-64 w-44 h-64 bg-gradient-to-br from-pink-400 to-violet-600 rounded-lg p-1 drop-shadow-[0_5px_5px_rgba(0,0,0,0.35)] left-0 top-0 transition-[left,top] duration-200 ease-in-out hover:-top-2 peer peer-hover:relative peer-hover:left-[72px]">
+		className="group hover:[transform:perspective(500px)_rotateY(var(--rot-y))_rotateX(var(--rot-x))] relative [flex -ml-[62px] first:ml-0 min-w-[15em] min-h-[21em] w-[15em] h-[21em] bg-gradient-to-br from-pink-400 to-violet-600 rounded-lg p-1 drop-shadow-[0_5px_5px_rgba(0,0,0,0.35)] left-0 top-0 transition-[left,top] duration-200 ease-in-out hover:-top-2 peer peer-hover:relative peer-hover:left-[72px]">
 			<div className="flex relative flex-col justify-between bg-[url('/assets/omega_triangle.jpg')] bg-[length:75%] brightness-[1.1] w-full h-full rounded-md p-2">
 				<div className={cardVariant == "holofoil" ? "absolute pointer-events-none h-full w-full rounded-lg top-0 left-0 bg-[linear-gradient(rgba(255,86,86,1)_0%,rgba(255,207,78,1)_15%,rgba(246,255,61,1)_20%,rgba(194,255,71,1)_35%,rgba(77,255,152,1)_52%,rgba(117,177,255,1)_69%,rgba(174,91,255,1)_85%,rgba(255,111,200,1)_97%),repeating-linear-gradient(133deg,#0e152e_0%,hsl(180,10%,60%)_3.8%,hsl(180,29%,66%)_4.5%,hsl(180,10%,60%)_5.2%,#0e152e_10%,#0e152e_12%)] bg-[length:100%_200%] bg-[0%_var(--chroma-shift),0%_var(--before-glare-shift)] bg-blend-hue brightness-[0.5] contrast-[2] saturate-[1.5] mix-blend-color-dodge after:content-[''] after:block after:absolute after:h-full after:w-full after:rounded-lg after:top-0 after:left-0 after:bg-[linear-gradient(rgba(255,86,86,1)_0%,rgba(255,207,78,1)_15%,rgba(246,255,61,1)_20%,rgba(194,255,71,1)_35%,rgba(77,255,152,1)_52%,rgba(117,177,255,1)_69%,rgba(174,91,255,1)_85%,rgba(255,111,200,1)_97%),repeating-linear-gradient(133deg,#0e152e_0%,hsl(180,10%,60%)_3.8%,hsl(180,29%,66%)_4.5%,hsl(180,10%,60%)_5.2%,#0e152e_10%,#0e152e_12%)] after:bg-[length:200%_300%] after:bg-[0%_var(--chroma-shift),-60px_var(--after-glare-shift)] after:bg-blend-hue after:brightness-[0.9] after:contrast-[1.2] after:saturate-[1.6] after:mix-blend-exclusion before:absolute before:h-full before:w-full before:rounded-lg before:top-0 before:left-0 before:bg-[url('/assets/omega_triangle_contrast.jpg')] before:bg-[length:75%] before:mix-blend-overlay" : "hidden"}/>
-				<div className={cardVariant == "polychrome" ? "absolute pointer-events-none h-full w-full top-0 left-0 rounded-lg bg-[url('/assets/omega_triangle_contrast.jpg'),repeating-radial-gradient(circle_at_var(--poly-x)_var(--poly-y),rgba(255,0,0,1),rgba(245,0,255,1),rgba(0,8,255,1),rgba(0,252,255,1),rgba(0,255,73,1),rgba(255,253,0,1),rgba(255,168,0,1),rgba(255,0,0,1)_130px)] bg-[length:75%,150%_100%] bg-blend-overlay brightness-[0.35] contrast-[1.2] saturate-[1.2] mix-blend-color-dodge" : "hidden"}/>
+				<div className={cardVariant == "polychrome" ? "absolute pointer-events-none h-full w-full top-0 left-0 rounded-lg bg-[url('/assets/omega_triangle_contrast.jpg'),repeating-radial-gradient(circle_at_var(--poly-x)_var(--poly-y),rgba(255,0,0,1),rgba(245,0,255,1),rgba(0,8,255,1),rgba(0,252,255,1),rgba(0,255,73,1),rgba(255,253,0,1),rgba(255,168,0,1),rgba(255,0,0,1)_180px)] bg-[length:75%,150%_100%] bg-blend-overlay brightness-[0.35] contrast-[1.2] saturate-[1.2] mix-blend-color-dodge" : "hidden"}/>
 				<button className="absolute hidden text-white bg-pink-700 group-hover:flex -right-3 -top-3 bg-pink-700 w-5 h-5 justify-center items-center rounded-full text-sm hover:bg-pink-500">x</button>
 				<div className="flex flex-col grow">
 					<div className={
@@ -56,7 +56,7 @@ const TCGCard = () => {
 					
 					<div className="bg-white my-2 h-2/5 rounded-md flex justify-center items-center bg-[url('https://i.pinimg.com/originals/6a/db/ea/6adbea3b760d8b09f2ea42d948e3db78.jpg')] bg-cover border-solid border-2 border-violet-700"/>
 
-					<div className="text-[10px] text-center select-none">
+					<div className="text-md text-center select-none">
 						Some very long descriptor text
 						here including some random information
 						about the specified card etc etc
--- osu_tcg/app/page.tsx
+++ osu_tcg/app/page.tsx
@@ -1,30 +1,40 @@
 "use client"
 import TCGCard from './_components/TCGCard/page.tsx';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 export default function Home() {
 	const [numRed, setNumRed] = useState(0);
 	const [numBlue, setNumBlue] = useState(0);
 	const numCardsPerRow = 9;
+
+	useEffect(() => {
+		const scaleFactor = Math.max(Math.min(-(1/8)*(numRed - 5) + 1, 1), 0.7);
+		document.body.style.setProperty("--row-scale-red", `${scaleFactor}`);
+	},[numRed]);
+	
+	useEffect(() => {
+		const scaleFactor = Math.max(Math.min(-(1/8)*(numBlue - 5) + 1, 1), 0.7);
+		document.body.style.setProperty("--row-scale-blue", `${scaleFactor}`);
+	},[numBlue]);
   return (
 	<main>
 		<div className="overflow-hidden bg-gradient-to-r from-red-500 from-40% to-blue-500 to-60% h-[960px] w-[2560px] min-h-[960px] min-w-[2560px] max-h-[960px] max-w-[2560px]">
 			<div className="grid grid-cols-2 h-full w-full">
-				<div className="my-2 py-4 flex flex-row justify-center items-center h-full w-full">
+				<div className="relative top-0 z-10 [transform:scale(var(--row-scale-red))] transition ease-out duration-200 py-4 flex flex-row justify-center items-center h-full w-full">
 					{[...Array(numRed)].fill(undefined).slice(0, numCardsPerRow).map((_) => <TCGCard/>)}
 				</div>
-				<div className="my-2 py-4 flex flex-row justify-center items-center h-full w-full">
+				<div className="relative top-0 z-10 [transform:scale(var(--row-scale-blue))] transition ease-out duration-200 py-4 flex flex-row justify-center items-center h-full w-full">
 					{[...Array(numBlue)].fill(undefined).slice(0, numCardsPerRow).map((_) => <TCGCard/>)}
 				</div>
-				<div className="my-2 py-4 flex flex-row justify-center items-center h-full w-full">
+				<div className="relative -top-28 [transform:scale(var(--row-scale-red))] transition ease-out duration-200 py-4 flex flex-row justify-center items-center h-full w-full">
 					{[...Array(numRed)].fill(undefined).slice(numCardsPerRow, 2 * numCardsPerRow).map((_) => <TCGCard/>)}
 				</div>
-				<div className="my-2 py-4 flex flex-row justify-center items-center h-full w-full">
+				<div className="relative -top-28 [transform:scale(var(--row-scale-blue))] transition ease-out duration-200 py-4 flex flex-row justify-center items-center h-full w-full">
 					{[...Array(numBlue)].fill(undefined).slice(numCardsPerRow,2 * numCardsPerRow).map((_) => <TCGCard/>)}
 				</div>
-				<div className="my-2 py-4 flex flex-row justify-center items-center h-full w-full">
+				<div className="relative -top-56 [transform:scale(var(--row-scale-red))] transition ease-out duration-200 py-4 flex flex-row justify-center items-center h-full w-full">
 					{[...Array(numRed)].fill(undefined).slice(2 * numCardsPerRow, 3 * numCardsPerRow).map((_) => <TCGCard/>)}
 				</div>
-				<div className="my-2 py-4 flex flex-row justify-center items-center h-full w-full">
+				<div className="relative -top-56 [transform:scale(var(--row-scale-blue))] transition ease-out duration-200 py-4 flex flex-row justify-center items-center h-full w-full">
 					{[...Array(numBlue)].fill(undefined).slice(2 * numCardsPerRow, 3 * numCardsPerRow).map((_) => <TCGCard/>)}
 				</div>
 			</div>
