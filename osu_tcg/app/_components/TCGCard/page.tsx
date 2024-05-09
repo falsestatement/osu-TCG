40ff6e095fee068a012057c8fda4cf1440d14afd
Adrian Cheng <bluepop11@hotmail.com>
2024-05-08 23:50:30 -0400

    feat: perspective shift on hover

--- osu_tcg/app/_components/TCGCard/page.tsx
+++ osu_tcg/app/_components/TCGCard/page.tsx
@@ -5,7 +5,7 @@ const TCGCard = () => {
 	// rarityVariants: common, uncommon, rare
 	const [counter, setCounter] = useState(0);
 	const boundingRef = useRef<DOMRect | null>(null);
-	const cardVariant = "holofoil";
+	const cardVariant = "polychrome";
 	const rarityVariant = "rare"
 
 	document.body.style.setProperty("--chroma-shift", '-200px');
@@ -31,16 +31,18 @@ const TCGCard = () => {
 			e.currentTarget.style.setProperty("--after-glare-shift", `${normX*240-240}px`);
 			e.currentTarget.style.setProperty("--poly-x", `${-(normX+2)*50}%`);
 			e.currentTarget.style.setProperty("--poly-y", `${-(normY+1)*70}%`);
+			e.currentTarget.style.setProperty("--rot-x", `${-(normY-0.5)*25}deg`);
+			e.currentTarget.style.setProperty("--rot-y", `${(normX-0.5)*25}deg`);
 
 			console.table({
 				MouseX: normX,
 				MouseY: normY,
 			});
 		}}
-		className="group relative [flex -ml-[62px] first:ml-0 min-w-44 min-h-64 w-44 h-64 bg-gradient-to-br from-pink-400 to-violet-600 rounded-lg p-1 drop-shadow-[0_5px_5px_rgba(0,0,0,0.35)] transition ease-in-out duration-200 hover:-translate-y-2 peer peer-hover:relative peer-hover:transition peer-hover:ease-in-out peer-hover:duration-200 peer-hover:translate-x-[72px]">
+		className="group hover:[transform:perspective(500px)_rotateY(var(--rot-y))_rotateX(var(--rot-x))] relative [flex -ml-[62px] first:ml-0 min-w-44 min-h-64 w-44 h-64 bg-gradient-to-br from-pink-400 to-violet-600 rounded-lg p-1 drop-shadow-[0_5px_5px_rgba(0,0,0,0.35)] left-0 top-0 transition-[left,top] duration-200 ease-in-out hover:-top-2 peer peer-hover:relative peer-hover:left-[72px]">
 			<div className="flex relative flex-col justify-between bg-[url('https://cdn.discordapp.com/attachments/1229567681217429659/1237587687079284796/omega_tringle.jpg?ex=663c30d9&is=663adf59&hm=863f802d21fad273ec64fd07adcb35d936b14fabe900c650fdca4e59bb042ca0&')] bg-[length:75%] brightness-[1.1] w-full h-full rounded-md p-2">
 				<div className={cardVariant == "holofoil" ? "absolute pointer-events-none h-full w-full rounded-lg top-0 left-0 bg-[linear-gradient(rgba(255,86,86,1)_0%,rgba(255,207,78,1)_15%,rgba(246,255,61,1)_20%,rgba(194,255,71,1)_35%,rgba(77,255,152,1)_52%,rgba(117,177,255,1)_69%,rgba(174,91,255,1)_85%,rgba(255,111,200,1)_97%),repeating-linear-gradient(133deg,#0e152e_0%,hsl(180,10%,60%)_3.8%,hsl(180,29%,66%)_4.5%,hsl(180,10%,60%)_5.2%,#0e152e_10%,#0e152e_12%)] bg-[length:100%_200%] bg-[0%_var(--chroma-shift),0%_var(--before-glare-shift)] bg-blend-hue brightness-[0.5] contrast-[2] saturate-[1.5] mix-blend-color-dodge after:content-[''] after:block after:absolute after:h-full after:w-full after:rounded-lg after:top-0 after:left-0 after:bg-[linear-gradient(rgba(255,86,86,1)_0%,rgba(255,207,78,1)_15%,rgba(246,255,61,1)_20%,rgba(194,255,71,1)_35%,rgba(77,255,152,1)_52%,rgba(117,177,255,1)_69%,rgba(174,91,255,1)_85%,rgba(255,111,200,1)_97%),repeating-linear-gradient(133deg,#0e152e_0%,hsl(180,10%,60%)_3.8%,hsl(180,29%,66%)_4.5%,hsl(180,10%,60%)_5.2%,#0e152e_10%,#0e152e_12%)] after:bg-[length:200%_300%] after:bg-[0%_var(--chroma-shift),-60px_var(--after-glare-shift)] after:bg-blend-hue after:brightness-[0.9] after:contrast-[1.2] after:saturate-[1.6] after:mix-blend-exclusion before:absolute before:h-full before:w-full before:rounded-lg before:top-0 before:left-0 before:bg-[url('https://cdn.discordapp.com/attachments/1229567681217429659/1237596606434639973/omega_triangle_contrast.jpg?ex=663c3928&is=663ae7a8&hm=159a8a1a73646e39bfd04dd2ded618d3d188d6ed5944172e6292a1673189e2c0&')] before:bg-[length:75%] before:mix-blend-exclusion" : "hidden"}/>
-				<div className={cardVariant == "polychrome" ? "absolute pointer-events-none h-full w-full top-0 left-0 rounded-lg bg-[url('https://cdn.discordapp.com/attachments/1229567681217429659/1237596606434639973/omega_triangle_contrast.jpg?ex=663c3928&is=663ae7a8&hm=159a8a1a73646e39bfd04dd2ded618d3d188d6ed5944172e6292a1673189e2c0&'),repeating-radial-gradient(75%_75%_at_var(--poly-x)_var(--poly-y),#FF0707FF,#FF00BDFF,#0024FFFF,#00FFF6FF,#B7FA70FF,#FFFE00FF,#FFA100FF,#FF0000FF_170px)] bg-[length:75%,150%_100%] bg-blend-overlay brightness-[0.4] contrast-[1.7] saturate-[1.2] mix-blend-color-dodge" : "hidden"}/>
+				<div className={cardVariant == "polychrome" ? "absolute pointer-events-none h-full w-full top-0 left-0 rounded-lg bg-[url('https://cdn.discordapp.com/attachments/1229567681217429659/1237596606434639973/omega_triangle_contrast.jpg?ex=663c3928&is=663ae7a8&hm=159a8a1a73646e39bfd04dd2ded618d3d188d6ed5944172e6292a1673189e2c0&'),repeating-radial-gradient(75%_75%_at_var(--poly-x)_var(--poly-y),rgba(255,86,86,1),rgba(255,207,78,1),rgba(246,255,61,1),rgba(194,255,71,1),rgba(77,255,152,1),rgba(117,177,255,1),rgba(174,91,255,1),rgba(255,111,200,1)_170px)] bg-[length:75%,150%_100%] bg-blend-overlay brightness-[0.4] contrast-[1.7] saturate-[1.2] mix-blend-color-dodge" : "hidden"}/>
 				<button className="absolute hidden text-white bg-pink-700 group-hover:flex -right-3 -top-3 bg-pink-700 w-5 h-5 justify-center items-center rounded-full text-sm hover:bg-pink-500">x</button>
 				<div className="flex flex-col grow">
 					<div className={
