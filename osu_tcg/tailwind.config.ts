19d2d386da5dd4cd1f66f749d15e1da979013150
Adrian Cheng <bluepop11@hotmail.com>
2024-05-12 10:35:03 -0400

    feat: idle animations

--- osu_tcg/tailwind.config.ts
+++ osu_tcg/tailwind.config.ts
@@ -18,6 +18,30 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+	"idle-wobble": {
+	  "0%, 100%": {transform: "perspective(500px) rotateZ(-1deg) rotateX(2deg) rotateY(-2deg) translateY(0px)"},
+	  "50%": {transform: "perspective(500px) rotateZ(1deg) rotateX(-2deg) rotateY(2deg) translateY(-5px)"},
+	},
+	"holo-idle-before": {
+	  "0%, 100%": {backgroundPosition: "0% -140px, 0% -10px"},
+	  "50%": {backgroundPosition: "0% -260px, 0% -50px"},
+	},
+	"holo-idle-after": {
+	  "0%, 100%": {backgroundPosition: "0% -140px, 0% -90px"},
+	  "50%": {backgroundPosition: "0% -260px, 0% -50px"},
+	},
+	"poly-idle": {
+	  "0%, 100%": {backgroundPosition: "-120px -98px"},
+	  "50%": {backgroundPosition: "-130px -112px"},
+	},
+      },
+      animation: {
+	"idle-wobble": "idle-wobble 3s ease-in-out infinite",
+	"holo-idle-before": "holo-idle-before 3s ease-in-out infinite",
+	"holo-idle-after": "holo-idle-after 3s ease-in-out infinite",
+	"poly-idle": "poly-idle 3s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
